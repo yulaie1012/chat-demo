c5bb3aa9da83d0b0fa7ff5c18e99dba2357e8034
Jason Luo <yulaie1012@gmail.com>
2021-12-30 13:45:38 +0800

    feat: add connect and disconnect event lisenter

--- server/server.js
+++ server/server.js
@@ -1,11 +1,24 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const socketIO = require("socket.io");
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '/../public')))
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`)
-})
+app.use(express.static(path.join(__dirname, "/../public")));
+
+io.on("connection", (socket) => {
+  console.log("A new user just connected");
+
+  socket.on("disconnect", () => {
+    console.log("User was disconnected");
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
